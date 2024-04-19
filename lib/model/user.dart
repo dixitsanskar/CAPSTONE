187d50de0a4a79cb5e478e194741d01ee9285513
vishesh0306 <115298750+vishesh0306@users.noreply.github.com>
2024-04-19 22:21:25 +0530

    user model done

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,5 +1,7 @@
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Users{
   String? id;
   String? name;
@@ -9,8 +11,28 @@ class Users{
   List<Collaboration>? collab_id;
   Users({this.id, this.collab_id, this.name, this.userName, this.github_id, this.resume});
 
+  Map<String, dynamic> tomap() {
+    return {
+      'id': id,
+      'collab_id': collab_id,
+      'name': name,
+      'userName': userName,
+      'github_id': github_id,
+      'resume': resume,
+    };
 
+  }
 
+  Users.fromMap(DocumentSnapshot snapshot){
+    Map<String,dynamic> data = snapshot.data() as Map<String,dynamic>;
+    id = data['id'];
+    collab_id = List<Collaboration>.from(data['collab_id']?.map((e)=> Collaboration.fromMap(e)));
+    name = data['name']??"";
+    userName = data['userName']??"";
+    github_id = data['github_id']??"";
+    resume = data['resume']??"";
+
+  }
 
 }
 
@@ -26,6 +48,31 @@ class Collaboration{
 
 
 
+  Map<String, dynamic> tomap() {
+    return {
+      'id': id,
+      'project_id': project_id,
+      'status': status,
+      'team_member_no': team_member_no,
+      'team_name': team_name,
+      'teammembers': teammembers,
+      'timeStamp': timeStamp,
+
+    };
+
+  }
+
+  Collaboration.fromMap(DocumentSnapshot snapshot){
+    Map<String,dynamic> data = snapshot.data() as Map<String,dynamic>;
+    id = data['id'];
+    project_id = data['project_id']??"";
+    status = data['status']??"";
+    team_member_no = data['team_member_no']??"";
+    team_name = data['team_name']??"";
+    teammembers = List<TeamMember>.from(data['teammembers']?.map((e)=> TeamMember.fromMap(e)));
+    timeStamp = data['timeStamp']??"";
+
+  }
 
 }
 
@@ -34,5 +81,19 @@ class TeamMember{
   String? name;
 
   TeamMember({this.id, this.name});
+
+  Map<String, dynamic> tomap() {
+    return {
+      'id': id,
+      'name': name,
+    };
+
+  }
+
+  TeamMember.fromMap(DocumentSnapshot snapshot){
+    Map<String,dynamic> data = snapshot.data() as Map<String,dynamic>;
+    id = data['id'];
+    name = data['name']??"";
+  }
 
 }
